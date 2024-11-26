c7ee67c47937109e1e7cc757c94be691991c0117
KhoaPham359 <145217965+KhoaPham359@users.noreply.github.com>
2024-11-26 15:37:11 +0700

    add student prefix into route

--- routes/student/studens.py
+++ routes/student/studens.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, jsonify
 from controllers.spso import printers_controller
 from middlewares.auth import student_is_required
+import os
 
 students_bp = Blueprint('students', __name__)
+student_prefix = os.getenv('STUDENT_PREFIX', 'student')
 
-@students_bp.route('/report_issue', methods=['POST'])
+@students_bp.route(f'/{student_prefix}/report_issue', methods=['POST'])
 @student_is_required
 def report_issue():
     data = request.get_json()
